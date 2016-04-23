3755efa7cfb39a580402af11ce0531a1f19e2c58
Kris Hermans <kris.hermans@live.be>
2016-04-23 10:09:31 +0200

    Removed unnecessary projects

---
+++ SensorTagDemo/SensorTagDemo/SensorTagLib/SensorTag.cs
@@ -0,0 +1,26 @@
+﻿using Robotics.Mobile.Core.Bluetooth.LE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorTagDemo.SensorTagLib
+{
+    /// <summary>
+    /// Following in the spirit of https://github.com/clovett/SensorTag-for-Windows
+    /// This class combines all of the GATT services provided by SensorTag into one helper class.
+    /// See http://processors.wiki.ti.com/index.php/CC2650_SensorTag_User's_Guide#IR_Temperature_Sensor
+    /// for details on GATT services.
+    /// Under the covers we will use https://github.com/xamarin/Monkey.Robotics
+    /// </summary>
+    public class SensorTag
+    {
+        private IDevice _device;
+
+        public SensorTag(IDevice device)
+        {
+            this._device = device;
+        }
+    }
+}
